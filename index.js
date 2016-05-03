1b7957353a71959d2ff24b0de2443e660829b3b8
Anthony Nguyen <anknguyen@gmail.com>
2016-05-03 15:25:29 -0400

    Add code for getting message by ID from Mongo

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+var mongo = require("mongodb");
+var MongoClient = mongo.MongoClient;
+var assert = require("assert");
+
+var db = null;
+
+var MONGO_URL = "mongodb://localhost:27017/audition";
+var COLLECTION = "messages";
+MongoClient.connect(MONGO_URL, function(err, _db) {
+	assert.equal(null, err);
+	console.log("Connected to mongodb server at " + MONGO_URL);
+	db = _db;
+});
+
+app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
 app.get("/get/:id", function (req, res) {
-	res.send(req.params.id);
+	// Cannot search _id with a string, you have to convert it to an ObjectId
+	var id = mongo.ObjectId(req.params.id);
+
+	db.collection(COLLECTION).findOne({_id: id}, function(err, doc) {
+		assert.equal(null, err);
+
+		if (doc != null) {
+			res.send(doc.message);
+		} else {
+			res.send("No message with that id");
+		}
+	});
 });
 
 app.listen(3000, function () {
