f35934593ad524ab87a8d69fecb77d925bac9fdc
Akib Shahjahan <AkibShahjahan@users.noreply.github.com>
2016-05-03 16:50:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get("/get/messages", function(req, res){	
+	db.collection(COLLECTION).find().toArray(function(err, messages){
+		if (err){
+        res.send(err);
+		} else
+		{
+        res.json(messages);
+		}
+	});
+});
+
 app.get("/get/:id", function (req, res) {
 	// Cannot search _id with a string, you have to convert it to an ObjectId
 	var id = mongo.ObjectId(req.params.id);
@@ -33,6 +44,7 @@ app.get("/get/:id", function (req, res) {
 		}
 	});
 });
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
