5a7f83b05a033ca1796e952f20fcf9c3da539494
Albert Yang <z274yang@gmail.com>
2016-05-06 15:15:52 -0400

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,7 +1,5 @@
-
-// Verifies whether a given message is a palindrome or not
-module.exports = function isPalindrome(message) {
-	if(message === null) return false;
+function isPalindrome(message) {
+	message = message.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase();
 	var len = message.length;
 	for ( var i = 0; i < Math.floor(len/2); i++) {
 		if (message[i] !== message[len - 1 - i]) {
